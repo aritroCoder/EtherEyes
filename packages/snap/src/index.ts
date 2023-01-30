ba72f05fa9aae22a4342c80e4825b569ec5d897d
aritroCoder <ogcodinggod@gmail.com>
2023-01-30 20:27:57 +0530

    added external APIs to fetch current gas data

--- packages/snap/src/index.ts
+++ packages/snap/src/index.ts
@@ -3,30 +3,17 @@ import {
   hasProperty,
   isObject,
   Json,
-  remove0x,
-  add0x,
-  bytesToHex,
 } from '@metamask/utils';
-import { decode } from '@metamask/abi-utils';
 
-// The API endpoint to get a list of functions by 4 byte signature.
-const API_ENDPOINT =
-  'https://www.4byte.directory/api/v1/signatures/?hex_signature=';
-
-/* eslint-disable camelcase */
-type FourByteSignature = {
-  id: number;
-  created_at: string;
-  text_signature: string;
-  hex_signature: string;
-  bytes_signature: string;
-};
-/* eslint-enable camelcase */
+// The API endpoint.
+const MODEL_API_ENDPOINT = 'http://localhost:5000/';
+const CURRENT_DATA_ENDPOINT = 'https://api.owlracle.info/v3/eth/gas?accept=70';
 
 export const onTransaction: OnTransactionHandler = async ({ transaction }) => {
   const insights: { type: string; params?: Json } = {
-    type: 'unknown transaction',
+    type: 'Gas Fee estimation',
   };
+
   if (
     !isObject(transaction) ||
     !hasProperty(transaction, 'data') ||
@@ -36,14 +23,14 @@ export const onTransaction: OnTransactionHandler = async ({ transaction }) => {
     return { insights };
   }
 
-  // parse the txn data
+  const response = await fetch(`${MODEL_API_ENDPOINT}`, {
+    method: 'get',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  });
 
-  const transactionData = remove0x(transaction.data);
-  const functionSignature = transactionData.slice(0, 8);
-
-  // fetch data from 4bytes
-
-  const response = await fetch(`${API_ENDPOINT}${add0x(functionSignature)}`, {
+  const currentData = await fetch(CURRENT_DATA_ENDPOINT, {
     method: 'get',
     headers: {
       'Content-Type': 'application/json',
@@ -51,60 +38,25 @@ export const onTransaction: OnTransactionHandler = async ({ transaction }) => {
   });
 
   if (!response.ok) {
-    throw new Error('4byte.directory API request failed');
+    throw new Error('API request failed');
   }
 
-  const { results } = (await response.json()) as {
-    results: FourByteSignature[];
+  const data = await response.json();
+  const current = await currentData.json();
+
+  insights.params = {
+    'Average Gas needed': current.avgGas,
+    'Current Min per Gas Fee (Gwei)': current.speeds[0].baseFee,
+    'Current Max per Gas Fee (Gwei)':
+      current.speeds[0].maxFeePerGas + current.speeds[0].maxPriorityFeePerGas,
+    'min fee required(Gwei)': current.avgGas * current.speeds[0].baseFee,
+    'max fee required(Gwei)':
+      current.avgGas *
+      (current.speeds[0].maxFeePerGas + current.speeds[0].maxPriorityFeePerGas),
+    'expected per gas price(Gwei)': data.expected_cost,
+    'expected total gas fee (Gwei)': data.expected_cost * current.avgGas,
+    'suggested waiting time': data.suggested_wait,
   };
 
-  const [functionTextSignature] = results
-    .sort((a, b) => a.created_at.localeCompare(b.created_at))
-    .map((value) => value.text_signature);
-
-  if (!functionTextSignature) {
-    console.warn('unknown transaction type');
-    return { insights };
-  }
-
-  insights.type = functionTextSignature;
-
-  // decode parameters
-
-  const parameterTypes = functionTextSignature
-    .slice(
-      functionTextSignature.indexOf('(') + 1,
-      functionTextSignature.indexOf(')'),
-    )
-    .split(',');
-
-  const decoded = decode(parameterTypes, add0x(transactionData.slice(8)));
-
-  insights.params = decoded.map(normalizeAbiValue)
-
   return { insights };
-  // return {insights}
 };
-
-/**
- * The ABI decoder returns certain which are not JSON serializable. This
- * function converts them to strings.
- *
- * @param value - The value to convert.
- * @returns The converted value.
- */
-function normalizeAbiValue(value: unknown): Json {
-  if (Array.isArray(value)) {
-    return value.map(normalizeAbiValue);
-  }
-
-  if (value instanceof Uint8Array) {
-    return bytesToHex(value);
-  }
-
-  if (typeof value === 'bigint') {
-    return value.toString();
-  }
-
-  return value as Json;
-}
